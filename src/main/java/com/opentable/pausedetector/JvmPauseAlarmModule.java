700403bcce81e1f17b05cff57d2ff9cecffa4166
Steven Schlansker <sschlansker@opentable.com>
2015-09-14 15:19:15 -0700

    Allow using IoC to set consume actions

--- src/main/java/com/opentable/pausedetector/JvmPauseAlarmModule.java
+++ src/main/java/com/opentable/pausedetector/JvmPauseAlarmModule.java
@@ -14,11 +14,19 @@
 package com.opentable.pausedetector;
 
 import java.time.Clock;
+import java.util.Set;
+import java.util.function.Consumer;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Binder;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Scopes;
+import com.google.inject.TypeLiteral;
+import com.google.inject.binder.LinkedBindingBuilder;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 import com.opentable.config.ConfigProvider;
 import com.opentable.lifecycle.Lifecycle;
@@ -26,11 +34,17 @@ import com.opentable.lifecycle.LifecycleStage;
 
 public class JvmPauseAlarmModule extends AbstractModule
 {
+    private static final String BIND_NAME = "_pauseAlarm";
+
     @Override
     public void configure()
     {
         bind (JvmPauseAlarm.class).toProvider(JvmPauseAlarmProvider.class).asEagerSingleton();
         bind (JvmPauseAlarmConfig.class).toProvider(ConfigProvider.of(JvmPauseAlarmConfig.class)).in(Scopes.SINGLETON);
+    }
+
+    public static LinkedBindingBuilder<Consumer<Long>> addAction(Binder binder) {
+        return Multibinder.newSetBinder(binder, new TypeLiteral<Consumer<Long>>() {}, Names.named(BIND_NAME)).addBinding();
     }
 
     @Override
@@ -49,6 +63,7 @@ public class JvmPauseAlarmModule extends AbstractModule
         private final JvmPauseAlarmConfig config;
         private final Lifecycle lifecycle;
         private Clock clock = Clock.systemUTC();
+        private Consumer<Long> action = l -> {};
 
         @Inject
         JvmPauseAlarmProvider(JvmPauseAlarmConfig config, Lifecycle lifecycle) {
@@ -61,9 +76,18 @@ public class JvmPauseAlarmModule extends AbstractModule
             this.clock = clock;
         }
 
+        @Inject(optional = true)
+        void setActions(@Named(BIND_NAME) Set<Consumer<Long>> actions) {
+            action = l -> {
+                for (Consumer<Long> a : actions) {
+                    a.accept(l);
+                }
+            };
+        }
+
         @Override
         public JvmPauseAlarm get() {
-            final JvmPauseAlarm alarm = new JvmPauseAlarm(clock::instant, config.getCheckTime().getMillis(), config.getPauseAlarmTime().getMillis(), l -> {});
+            final JvmPauseAlarm alarm = new JvmPauseAlarm(clock::instant, config.getCheckTime().getMillis(), config.getPauseAlarmTime().getMillis(), action);
             if (config.isPauseAlarmEnabled()) {
                 lifecycle.addListener(LifecycleStage.CONFIGURE_STAGE, alarm::start);
                 lifecycle.addListener(LifecycleStage.STOP_STAGE, alarm::close);
