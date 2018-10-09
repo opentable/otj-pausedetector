ebe0ab1aafd0e2fc1c18cce412d93cb33a45ca95
Adam Richeimer <adamzr@gmail.com>
2018-10-09 16:36:45 -0700

    Add JavaDoc for all public classes and methods

--- src/main/java/com/opentable/pausedetector/EnablePauseDetector.java
+++ src/main/java/com/opentable/pausedetector/EnablePauseDetector.java
@@ -20,6 +20,9 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Import;
 
+/**
+ * Configures, creates, and starts a JVM Pause Alarm to monitor for long JVM pauses and log them
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Import(JvmPauseAlarmConfiguration.class)
